0c7da29bc47faf42f38a313415ae40de9d3a1a7f
FengheTan9 <543759045@qq.com>
2025-02-16 13:12:47 +0800

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import argparse
 import torch
 import random
 import numpy as np
-from trainer import DenseMAETrainer
+from trainer import HiEndMAETrainer
 
 cpu_num = 1
 os.environ['OMP_NUM_THREADS'] = str(cpu_num)
@@ -17,7 +17,7 @@ torch.set_num_threads(cpu_num)
 torch.multiprocessing.set_sharing_strategy('file_system')
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, default="dense_mae_vit_large_16p", choices=["dense_mae_vit_large_12p", "dense_mae_vit_base_16p", "dense_mae_vit_large_16p"], help='model')
+parser.add_argument('--model', type=str, default="hi)hi_end_mae_vit_large_12p", choices=["hi_end_mae_vit_large_12p", "hi_end_mae_vit_base_16p", "hi_end_mae_vit_large_16p"], help='model')
 parser.add_argument('--base_dir1', type=str, default="", help='dir')
 parser.add_argument('--base_lr', type=float, default=0.0001, help='learning rate')
 parser.add_argument('--batch_size', type=int, default=24, help='batch_size per gpu')
@@ -28,7 +28,7 @@ parser.add_argument('--in_chans', type=int, default=1, help='input channels')
 parser.add_argument('--pos_embed_type', type=str, default="sincos")
 parser.add_argument('--seed', type=int, default=41, help='random seed')
 parser.add_argument('--print_freq', type=int, default=50)
-parser.add_argument('--ckpt_dir', type=str, default="./ckpt/dense_mae_vit_large_12p")
+parser.add_argument('--ckpt_dir', type=str, default="./ckpt/hi_end_mae_vit_large_12p")
 parser.add_argument('--warmup_epochs', type=int, default=10)
 parser.add_argument('--beta1', type=float, default=0.9)
 parser.add_argument('--beta2', type=float, default=0.95)
